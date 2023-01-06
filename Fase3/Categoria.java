fff665dfcc86935464ccb665d855e6196250a0cf
Beatriz Monteiro <73402868+5ditto@users.noreply.github.com>
2023-01-06 22:54:04 +0000

    User + categoria incompleta

---
+++ Fase3/Categoria.java
@@ -0,0 +1,24 @@
+public class Categoria {
+    
+}
+
+class C1 extends Categoria{
+    private Boolean hibrido;
+    private int redFiabilidade;
+    private int aumentoPotencia;
+
+
+    
+}
+
+class C2 extends Categoria{
+
+}
+
+class GT extends Categoria{
+
+}
+
+class SC extends Categoria{
+
+}
