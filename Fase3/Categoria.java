8250ae02a05b9d68b6c7f05b21c6cc5ff8d9d82c
29bianca <biancavale2003@gmail.com>
2023-01-06 23:01:42 +0000

    mais cenas para a categoria

--- Fase3/Categoria.java
+++ Fase3/Categoria.java
@@ -7,16 +7,18 @@ class C1 extends Categoria{
     private int redFiabilidade;
     private int aumentoPotencia;
 
-
-    
 }
 
 class C2 extends Categoria{
-
+    private Boolean hibrido;
+    private int redFiabilidade;
+    private int aumentoPotencia;
 }
 
 class GT extends Categoria{
-
+    private Boolean hibrido;
+    private int redFiabilidade;
+    private int aumentoPotencia;
 }
 
 class SC extends Categoria{
