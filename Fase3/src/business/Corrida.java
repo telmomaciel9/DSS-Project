2bedabb3110ead7dda7914f9642e0ee3ab0b11de
Beatriz Monteiro <73402868+5ditto@users.noreply.github.com>
2023-01-07 00:32:26 +0000

    corrida começo

---
+++ Fase3/src/business/Corrida.java
@@ -0,0 +1,20 @@
+package business;
+
+import java.util.List;
+
+public class Corrida {
+    private List<User> classCorrida;
+
+    public void simulacao(){}
+    public void simulacaoPremium(){}
+    public List<User> getClassCorrida(){ return null;}
+    public Boolean ultrapassagem(User user1, User u2, int gdu, int diferenca){
+        return false;
+    }
+    public void newCorrida(User user, Carro carro, ModoMotor modoMotor, TipoPneu pneu, float pac){
+
+    }
+
+    publicPosicao
+
+}
