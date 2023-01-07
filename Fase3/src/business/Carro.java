2d88c3b9134718e4c11ab60e9a5d1f4436f885a1
29bianca <biancavale2003@gmail.com>
2023-01-07 00:20:17 +0000

    carro done

--- Fase3/src/business/Carro.java
+++ Fase3/src/business/Carro.java
@@ -5,24 +5,36 @@ public class Carro implements Comparable<Carro> {
     private float pac;
     private int potencia;
     private String marca;
+    private String modelo;
+    private String tipoPneu;
+    private String modoMotor;
 
     /* Construtores */
     public Carro() {
         this.pac = 0;
         this.potencia = 0;
         this.marca = "";
+        this.modelo = "";
+        this.tipoPneu = "";
+        this.modoMotor = "";
     }
 
-    public Carro(float pac, int potencia, String marca) {
+    public Carro(float pac, int potencia, String marca, String modelo, String pneu, String motor) {
         this.pac = pac;
         this.potencia = potencia;
         this.marca = marca;
+        this.modelo = modelo;
+        this.tipoPneu = pneu;
+        this.modoMotor = motor;
     }
 
     public Carro(Carro c) {
         this.pac = c.getPac();
         this.potencia = c.getPotencia();
         this.marca = c.getMarca();
+        this.modelo = c.getModelo();
+        this.tipoPneu = c.getTipoPneu();
+        this.modoMotor = c.getModoMotor();
     }
 
     /* Gets e sets */
@@ -38,6 +50,17 @@ public class Carro implements Comparable<Carro> {
         return this.marca;
     }
 
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public String getTipoPneu(){
+        return this.tipoPneu;
+    }
+
+    public String getModoMotor(){
+        return this.modoMotor;
+    }
 
     public void setPac(float pac) {
         this.pac = pac;
@@ -51,7 +74,18 @@ public class Carro implements Comparable<Carro> {
         this.marca = marca;
     }
 
-    
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setTipoPneu(String tipoPneu) {
+        this.tipoPneu = tipoPneu;
+    }
+
+    public void setModoMotor(String modoMotor) {
+        this.modoMotor = modoMotor;
+    }
+
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -61,6 +95,12 @@ public class Carro implements Comparable<Carro> {
         sb.append(this.potencia);
         sb.append("\nMarca: ");
         sb.append(this.marca);
+        sb.append("\nModelo: ");
+        sb.append(this.modelo);
+        sb.append("\nTipo Pneu: ");
+        sb.append(this.tipoPneu);
+        sb.append("\nModo Motor: ");
+        sb.append(this.modoMotor);
         return sb.toString();
     }
 
@@ -74,11 +114,14 @@ public class Carro implements Comparable<Carro> {
         Carro c = (Carro) o;
         return (this.pac == (c.getPac()) &&
                 this.potencia == (c.getPotencia()) &&
-                this.marca.equals(c.getMarca()));
+                this.marca.equals(c.getMarca()) &&
+                this.modelo.equals(c.getModelo()) &&
+                this.modelo.equals(c.getTipoPneu()) &&
+                this.modelo.equals(c.getModoMotor()));
     }
 
     public int compareTo(Carro c) {
         return this.marca.compareTo(c.getMarca());
     }
 }
-}
+
